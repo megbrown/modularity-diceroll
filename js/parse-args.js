6652d506538fce178e996c00ffa75383ca58bb70
Megan B <brown.m.j@outlook.com>
2017-08-30 11:36:10 -0500

    parse-args file should export a single function to parse your command line arguments, convert to object

---
+++ js/parse-args.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let [,, ...myArgs] = process.argv;
+
+module.exports = (myArgs) => {
+	if (myArgs) {
+		let [count, sides] = myArgs;
+		const diceObj = { count, sides };
+	} else {
+		console.log("something");
+	}
+
+}
