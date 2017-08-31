69531064a61ff306711e3251b940cc07fae7b3fe
Megan B <brown.m.j@outlook.com>
2017-08-31 15:59:28 -0500

    first testing

---
+++ test/math.test.js
@@ -0,0 +1,14 @@
+const { assert: { isFunction, isNumber } } = require('chai');
+const randomInt = require('../js/math');
+
+describe("math", () => {
+	describe("math a function", () => {
+		it("should exist", () => {
+			isFunction(randomInt);
+		});
+
+		it("should return an number", () => {
+			isNumber(randomInt());
+		});
+	});
+});
